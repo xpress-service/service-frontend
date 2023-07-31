e1dc5ebe21e5724c263082bdbeca8d32658f500a
descmd1 <aondoakaachris@gmail.com>
2023-07-31 10:12:27 +0100

    third commit

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,28 +1,25 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
   const initialValues = {
-    emailOrPhone: '',
-    password: '',
+    emailOrPhone: "",
+    password: "",
   };
 
   const validationSchema = Yup.object().shape({
-    emailOrPhone: Yup.string().required('Email or phone number is required'),
-    password: Yup.string().required('Password is required'),
+    emailOrPhone: Yup.string().required("Email or phone number is required"),
+    password: Yup.string().required("Password is required"),
   });
 
   const handleLogin = (values) => {
-    console.log('Login details:', values);
+    console.log("Login details:", values);
     // Here you can handle the actual login logic (e.g., sending data to the server)
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-        </div>
         <Formik
           initialValues={initialValues}
           validationSchema={validationSchema}
@@ -40,22 +37,30 @@ const Login = () => {
                   placeholder="Email or Phone Number"
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border text-gray-700 placeholder-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 />
-                <ErrorMessage name="emailOrPhone" component="div" className="text-red-500 text-sm" />
+                <ErrorMessage
+                  name="emailOrPhone"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
               </div>
               <div className="mt-3">
                 {/* <label htmlFor="password" className="sr-only">Password</label> */}
                 <Field
                   id="password"
                   name="password"
-                //   type="password"
-                //   autoComplete="current-password"
+                  //   type="password"
+                  //   autoComplete="current-password"
                   placeholder="password"
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border text-gray-700 placeholder-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 />
-                <ErrorMessage name="password" component="div" className="text-red-500 text-sm" />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
               </div>
             </div>
-            
+
             <div>
               <button
                 type="submit"
@@ -67,20 +72,19 @@ const Login = () => {
                 Login
               </button>
             </div>
-              <div className="flex items-center">
-                <p className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                  Forgotten password?
-                </p>
-              </div>
-              <div className="text-sm">
-                <span className="text-gray-500">Or</span>
-              </div>
-              <div className="flex items-center">
-                <button className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-stone-800 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                 >
-                 Create an account
-                </button>
-              </div>
+            <div className="flex items-center">
+              <p className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                Forgotten password?
+              </p>
+            </div>
+            <div className="text-sm">
+              <span className="text-gray-500">Or</span>
+            </div>
+            <div className="flex items-center">
+              <button className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-stone-800 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Create an account
+              </button>
+            </div>
           </Form>
         </Formik>
       </div>
