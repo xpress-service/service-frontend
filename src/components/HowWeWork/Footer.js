2f098e397d0c5decfcb1ed8d6388590a3873a1ee
descmd1 <aondoakaachris@gmail.com>
2023-09-08 18:10:50 +0100

    added landing page

---
+++ src/components/HowWeWork/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex flex-row bg-amber-600 gap-52 justify-center items-center'>
+        <div className='text-white text-md flex flex-col gap-3'>
+        <p className='mb-3 mt-3 font-bold'>Company</p>
+        <p>About Us</p>
+        <p>Desyination</p>
+        <p>Package</p>
+        <p>Contact</p>
+        </div>
+
+        <div className='text-white text-md flex flex-col gap-3'>
+        <p className='mb-3 font-bold'>Help</p>
+        <p>Help/FAQ</p>
+        <p>Cancel your order</p>
+        <p>Press</p>
+        </div>
+
+        <div className='text-white text-md flex flex-col gap-3'>
+        <p className='mb-3 mt-3 font-bold'>More</p>
+        <p>Block</p>
+        <p>Investor Relations</p>
+        <p>Partnerships</p>
+        <p>Jobs</p>
+        </div>
+
+        <div className='text-white text-md flex flex-col gap-3'>
+        <p className='mb-3 font-bold'>Terms</p>
+        <p>Privacy policy</p>
+        <p>Terms of use</p>
+        <p>Accesibility</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
